3209b867a8421285989a9c8215eb18c2028e4e26
Cheng Li <scrappedprince.li@gmail.com>
2018-07-18 14:07:52 +0800

    update backtest example

--- examples/backtest.jl
+++ examples/backtest.jl
@@ -1,32 +1,21 @@
 include("../src/AlgoTrading.jl")
 
+using CSV
 using AlgoTrading
 using DataFrames
 using Base.DateTime
 
-df = readtable("d:/sample.csv")
-close_table = unstack(df, :trade_time, :pair, :close)
-dropmissing!(close_table)
+close_table = readtable("d:/binance_eth_eos_pivot.csv", normalizenames=false)
 close_table[:trade_time] =
     map((x::String) -> DateTime(x, "yyyy-mm-dd HH:MM:SS"), close_table[:trade_time])
 
-currencies = ["BTC", "EOS", "USDT"]
+currencies = ["ETH", "EOS", "USDT"]
 
 cols = [string(currencies[1], "|", currencies[3]),
         string(currencies[2], "|", currencies[3]),
         string(currencies[2], "|", currencies[1])]
 
 n, m = size(close_table)
-
-comm = PerVolume(0.0005)
-amount = 500. # in USDT
-
-balance = Balance([Currency(c) for c in currencies])
-portfolio = DataFrame(trade_time=close_table[:trade_time],
-                      BTC=zeros(n),
-                      EOS=zeros(n),
-                      USDT=zeros(n))
-cash_names = names(portfolio)[2:end]
 
 function updatebalance!(balance::Balance, incashes, outcashes, comms)
     for cash in incashes
@@ -42,40 +31,63 @@ function updatebalance!(balance::Balance, incashes, outcashes, comms)
     end
 end
 
-@time @parallel for i in 1:n
-    row = close_table[i, :]
-    quote1 = FXQuote(FXPair(cols[1]), row[Symbol(cols[1])][1])
-    quote2 = FXQuote(FXPair(cols[2]), row[Symbol(cols[2])][1])
-    quote3 = FXQuote(FXPair(cols[3]), row[Symbol(cols[3])][1])
+for bps in [5, 7, 10, 12]
 
-    arbitrage = quote1 / quote2 * quote3
+    comm = PerVolume(bps / 10000.)
+    amount = 500. # in USDT
 
-    if arbitrage.value > 1.003
-        # 正向套利机会
-        incash1, outcash1, comm1 = sell(quote1, amount/ quote1.value, comm)
-        incash2, outcash2, comm2 = sell(quote3, -outcash1.value / quote3.value, comm)
-        incash3, outcash3, comm3 = buy(quote2, incash1.value / quote2.value, comm)
-        incashes = [incash1, incash2, incash3]
-        outcashes = [outcash1, outcash2, outcash3]
-        comms = [comm1, comm2, comm3]
-        updatebalance!(balance, incashes, outcashes, comms)
-    elseif arbitrage.value < 0.997
-        # 反向套利机会
-        incash1, outcash1, comm1 = sell(quote2, amount / quote2.value, comm)
-        incash2, outcash2, comm2 = buy(quote3, -outcash1.value, comm)
-        incash3, outcash3, comm3 = buy(quote1, incash1.value / quote1.value, comm)
-        incashes = [incash1, incash2, incash3]
-        outcashes = [outcash1, outcash2, outcash3]
-        comms = [comm1, comm2, comm3]
-        updatebalance!(balance, incashes, outcashes, comms)
+    initial = Dict(currencies[1] => Cash(Currency(currencies[1]), 0.),
+                   currencies[2] => Cash(Currency(currencies[2]), 0.),
+                   currencies[3] => Cash(Currency(currencies[3]), amount))
+
+    balance = Balance(initial)
+    portfolio = DataFrame(:trade_time => close_table[:trade_time],
+                          Symbol(currencies[1]) => zeros(n),
+                          Symbol(currencies[2]) => zeros(n),
+                          Symbol(currencies[3]) => zeros(n))
+    cash_names = names(portfolio)[2:end]
+
+
+
+    @time @parallel for i in 1:n
+        row = close_table[i, :]
+        quote1 = FXQuote(FXPair(cols[1]), row[Symbol(cols[1])][1])
+        quote2 = FXQuote(FXPair(cols[2]), row[Symbol(cols[2])][1])
+        quote3 = FXQuote(FXPair(cols[3]), row[Symbol(cols[3])][1])
+
+        arbitrage = quote1 / quote2 * quote3
+
+        if arbitrage.value > 1.003
+            # 正向套利机会
+            incash1, outcash1, comm1 = sell(quote1, amount/ quote1.value, comm)
+            incash2, outcash2, comm2 = sell(quote3, -outcash1.value / quote3.value, comm)
+            #incash3, outcash3, comm3 = buy(quote2, incash1.value / quote2.value, comm)
+            incash3, outcash3, comm3 = buy(quote2, -outcash2.value, comm)
+            incashes = [incash1, incash2, incash3]
+            outcashes = [outcash1, outcash2, outcash3]
+            comms = [comm1, comm2, comm3]
+            updatebalance!(balance, incashes, outcashes, comms)
+        elseif arbitrage.value < 0.997
+            # 反向套利机会
+            incash1, outcash1, comm1 = sell(quote2, amount / quote2.value, comm)
+            incash2, outcash2, comm2 = buy(quote3, -outcash1.value, comm)
+            #incash3, outcash3, comm3 = buy(quote1, incash1.value / quote1.value, comm)
+            incash3, outcash3, comm3 = buy(quote1, -outcash2.value, comm)
+            incashes = [incash1, incash2, incash3]
+            outcashes = [outcash1, outcash2, outcash3]
+            comms = [comm1, comm2, comm3]
+            updatebalance!(balance, incashes, outcashes, comms)
+        end
+
+        for k in 1:length(cols)
+            portfolio[i, Symbol(cash_names[k])] = getbalance(balance, string(cash_names[k])).value
+        end
+
     end
 
-    for k in 1:length(cols)
-        portfolio[i, Symbol(cash_names[k])] = getbalance(balance, string(cash_names[k])).value
+    for name in cols[1:end-1]
+        portfolio[Symbol(name)] = close_table[Symbol(name)]
     end
 
-end
-
-for name in cols[1:end-1]
-    portfolio[Symbol(name)] = close_table[Symbol(name)]
+    CSV.write("d:/arb_$(bps)bps.csv", portfolio, dateformat="yyyy-mm-dd HH:MM:SS")
 end
