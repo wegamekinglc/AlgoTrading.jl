a8557ccb65c8daec576c4cfa2c25f7a1b68b3f6a
Cheng Li <scrappedprince.li@gmail.com>
2018-07-31 16:07:44 +0800

    initial data api stuff

---
+++ src/datas/bars.jl
@@ -0,0 +1,34 @@
+using HTTP
+using DataFrames
+using Base.Dates
+
+function getbar(pair="ltc_btc", mintype="1min", barsize=10)
+    resp = HTTP.get("https://www.okex.com/api/v1/kline.do?symbol=$pair&type=$mintype&size=$barsize");
+    text = String(resp.body)
+
+    mat = maketable(text, 6)
+    df = DataFrame(mat, [:trade_time, :open, :high, :low, :close, :volume])
+    df[:trade_time] =
+        map((x) -> unix2datetime(x / 1000.), df[:trade_time])
+    df
+end
+
+
+function parserow!(i::Int, s::AbstractString, mat::Array{Float64, 2})
+    items = split(strip(s, ['[', ']']), ",")
+    n = length(items)
+    for j in 1:n
+        mat[i, j] = float(strip(items[j], '\"'))
+    end
+    nothing
+end
+
+function maketable(text::String, fields::Int)
+    matches = matchall(r"\[[0-9,.\"\ ]+\]", text)
+    n = length(matches)
+    mat = Array{Float64, 2}(n, fields)
+    for i in 1:n
+        parserow!(i, matches[i], mat)
+    end
+    mat
+end
